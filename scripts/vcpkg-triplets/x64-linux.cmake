9c9fac2a2b515a4491dcd9ffda04a2922c43cdee
keejkrej <ctyjackcao@outlook.com>
2026-04-24 18:31:00 +0200

    Fix Linux minc vcpkg build

--- scripts/vcpkg-triplets/x64-linux.cmake
+++ scripts/vcpkg-triplets/x64-linux.cmake
@@ -3,3 +3,7 @@ include("${VCPKG_ROOT_DIR}/triplets/x64-linux.cmake")
 if(PORT STREQUAL "liblzma")
   list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS -DXZ_ARM64_CRC32=OFF)
 endif()
+
+if(PORT STREQUAL "minc")
+  string(APPEND VCPKG_C_FLAGS " -std=gnu17")
+endif()
